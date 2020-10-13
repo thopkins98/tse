0f26a224eb8d1aa3af81dd6e760d95b047c2f58e
Trevor Hopkins <trevorscotthopkins@gmail.com>
2020-10-13 16:42:45 -0400

    initial commit

---
+++ crawler/crawler.c
@@ -0,0 +1,18 @@
+/* crawler.c --- 
+ * 
+ * 
+ * Author: Trevor S. Hopkins
+ * Created: Tue Oct 13 16:13:13 2020 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+
+int main(void){
+
+	printf("hello\n");
+
+	return 0;
+}
