6d78da64b9c76ed1b576321a31610db376b0cc9a
Trevor Hopkins <trevorscotthopkins@gmail.com>
2020-10-16 19:00:20 -0400

    first pass

--- crawler/crawler2.c
+++ crawler/crawler2.c
@@ -12,24 +12,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
 
 int main(void){
 
 	printf("hello\n");
 
 	//const int depth = 0;
-	//char *seed = "https://thayer.github.io/engs50/";
-	//char *html1 = NULL;
+	char *seed = "https://thayer.github.io/engs50/";
 	
-	webpage_t *w1 = NULL;
-	w1 = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
+	struct webpage *w1 = webpage_new(seed, 0, NULL);
+	struct queue *pagequeue = qopen();
+		
 	bool check = false;
-
-	printf("check\n");
-	
 	check = webpage_fetch(w1);
-	
+
 	if (check == false) {
 		exit(EXIT_FAILURE);
 	}
@@ -37,6 +33,9 @@ int main(void){
 	int pos=0;
 	char *result;
 	bool internal = false;
+
+	int counter = 1;
+	
 	
 	while ((pos = webpage_getNextURL(w1, pos, &result)) > 0) {
 
@@ -44,13 +43,29 @@ int main(void){
 		printf("Found url: %s", result);
 		if (internal == true) {
 			printf(" This URL is Internal\n");
+			struct webpage *page = webpage_new(result, counter, NULL);
+			qput(pagequeue, (void*)page);
 		}else {
 			printf(" This URL is not Internal\n");
 		}
 		free (result);
+		webpage_delete((void *)page);
 	}
 
 	webpage_delete((void *)w1);
+	
+	//print queue here
+
+	struct webpage *printpage = NULL;
+	char *urltoprint = NULL;
+	while (pagequeue->front != NULL){
+		printpage = (webpage_t *)qget(pagequeue);
+		urltoprint = printpage->url;
+		fprint("%s\n", urltoprint);
+		webpage_delete((void *)printpage);
+	}
+
+	qclose(pagequeue);
 	
 	return 0;
 }
