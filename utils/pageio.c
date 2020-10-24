681fc8a7cdfe9d590203f1b092d5e3b661e2ae41
isurucs50 <isuru.abudana123@hotmail.com>
2020-10-24 11:00:00 -0400

    Initial pageload function

--- utils/pageio.c
+++ utils/pageio.c
@@ -12,12 +12,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "webpage.h"
+#include <webpage.h>
 #include "hash.h"
 #include "queue.h"
 #include <string.h>
 #include <stdint.h>
 #include <inttypes.h>
+
+
+static char* full_dirname( char* dirnm, int id );
 
 /*
 
@@ -30,6 +33,7 @@ dirnm: directory where the file is saved.
 
 
  */
+
 int32_t pagesave(webpage_t *pagep, int id, char*dirnm) {
 
 	FILE *fp;
@@ -47,5 +51,55 @@ int32_t pagesave(webpage_t *pagep, int id, char*dirnm) {
 	fclose(fp);
 
 	return 0;
+
+}
+
+
+
+
+webpage_t* pageload(int id, char* dirnm) {
+
+	char* fdir= full_dirname(dirnm, id);
+	
+	FILE *input= fopen(fdir, "r");
+
+	char *url= (char *) malloc(sizeof(char *));
+	
+	fscanf(input, "%s", url);
+	int depth;
+	fscanf(input, "%d", &depth);
+	int html_len;
+	fscanf(input, "%d", &html_len);
+	//int c=0;
+	char html[html_len];
+	
+	while ( !feof(input)) {
+		char c= fgetc(input);
+		strcat(html, &c); 
+	}
+
+	webpage_t *res= webpage_new(url, depth, html);
+
+	fclose(input);
+	free(url);
+
+	return res;
+
+	
+}
+
+static char* full_dirname( char* dirnm, int id ) {
+	// This checks how much space must be allocate for id in array
+	int ENOUGH=snprintf(NULL, 0, "%d", id);
+	char num[ENOUGH];
+	sprintf(num, "%d", id);
+	char slash[2]= "/";
+
+	char* dir_end= strcat(slash, num);
+
+	char* res= strcat(dirnm, dir_end);
+
+	return res;
+
 
 }
