618b52dda7f957a7f0994428ad13cc419bcd9af2
Trevor Hopkins <trevorscotthopkins@gmail.com>
2020-10-25 22:27:49 -0400

    step 3 done

---
+++ indexer/indexer_step3.c
@@ -0,0 +1,109 @@
+/* indexer.c --- 
+1;95;0c * 
+ * 
+ * Author: Agampodi I. Abeysekara
+ * Created: Sun Oct 25 11:25:16 2020 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "webpage.h"
+#include "pageio.h"
+#include <string.h>
+#include <ctype.h>
+#include "hash.h"
+#include "queue.h"
+
+typedef struct word{
+	char *word;
+	int count;
+} word_t;
+
+char* NormalizeWord(char *str);
+bool searchfunc(void* elementp, const void* searchkeyp);
+bool s(void* p, const void* key);
+void sumwords(void *ep);
+
+//global variable
+int sum=0;
+
+int main(void) {
+	webpage_t *w1= pageload(1, "../pages");
+	word_t *wordpointer = (word_t *)malloc(sizeof(word_t));
+	word_t *wordsearch = (word_t *)malloc(sizeof(word_t));
+	hashtable_t *ht = hopen(100);
+	
+	char *result;
+	int pos=0;
+	while ((pos = webpage_getNextWord(w1, pos, &result)) > 0 ) {
+		if (strcmp(NormalizeWord(result), "") != 0) {
+			if ((wordsearch = (word_t *)hsearch(ht, s, result, strlen(result))) != NULL){
+				wordsearch->count = wordsearch->count + 1;	
+				free(result);
+			}else{
+				wordpointer->word = result;
+				wordpointer->count = 1;
+				hput(ht, (void *)wordpointer, wordpointer->word, strlen(wordpointer->word));
+				free(result);
+			}
+		}
+	  else {
+			free(result);
+		  continue;
+		}
+		
+	}
+
+	free(wordpointer);
+	webpage_delete(w1); 
+
+	//report sum
+	happly(ht, sumwords);
+	printf("Total Word Count: %d\n", sum);
+	
+	return 0;
+}
+
+void sumwords(void *ep){
+	word_t *w = (word_t *)ep;
+	sum=sum + w->count;
+	return;
+}
+
+bool s(void* p, const void* key){
+
+	word_t *searchw = (word_t*)p;
+	char *w = searchw->word;
+
+	if (strcmp(w, key) == 0){
+		return true;
+	}
+
+	return false;
+}
+	
+char* NormalizeWord(char *str) {
+	int i=0;
+	bool check= false;
+	while (str[i] != '\0') {
+		str[i]= tolower(str[i]);
+		int ascii= (int) str[i];
+		if (ascii >= 97 && ascii <= 122) {
+			check= true;
+		}
+		else {
+			check= false;
+		}
+		i++;
+	}
+
+	if (check == false || strlen(str) < 3) {
+		return "";
+	}
+	return str;
+}
