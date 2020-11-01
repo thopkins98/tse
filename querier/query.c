d286853133c2019abd4d93060ededf9a51c78978
isurucs50 <isuru.abudana123@hotmail.com>
2020-11-01 13:38:24 -0500

    Step 1 bugs fixed

--- querier/query.c
+++ querier/query.c
@@ -18,53 +18,61 @@ int main(void) {
 	
 	printf(">");
 	while(fgets(buffer, 100, stdin)) {
-		int len= strlen(buffer);
-
-		// The text array would hold the parsed query. Used calloc which initializes pointer to zero bytes.
-		
-		char* text= (char *) calloc(len + 1, sizeof(char)) ;
-
-		// Checks if the return key was pressed
-		
 		if (strcmp(buffer, "\n") == 0) {
-			printf(">\n");
 			printf(">");
-			continue;
 		}
 
-		// Building the text array with the count index.
+		char *word;
+		char *ptr= buffer;
+
+		char *word_arr[1024];
 		
 		int count=0;
+		int punc_check=0;
 
-		for (int i=0; i<len; i++) {
-			char c= buffer[i];
+		while ((word= strtok(ptr, " \t\n"))) {
+			ptr= NULL;
 
-			// Checks for spaces and tabs using ASCII codes
+			for (int i=0; i< strlen(word); i++) {
+				char c= word[i];
 			
-			if ((int) c == 32 || (int) c ==9) {
-				continue;
-			}
+				if (((int) c >= 33 && (int) c <= 47) || ((int) c >= 58 && (int) c <= 64) || ((int) c >= 91 && (int) c <= 96) || ((int) c >= 123 && (int) c <= 126)) {
 
-			// Checks for punctuation marks using ASCII codes
-			
-			if (((int) c >= 33 && (int) c <= 47) || ((int) c >= 58 && (int) c <= 64) || ((int) c >= 91 && (int) c <= 96) || ((int) c >= 123 && (int) c <= 126)) {
-					printf("[invalid query]\n");
-
-					//text prints letters in the buffer up to the point the punctuation was first detected so its converted to an empty string
-					strcpy(text, "\0");
+					punc_check=1;
 					break;
 				}
+				
 
 			// Converting to lower case here
 			
-				text[count]= tolower(c);
-				count++;
+				word[i]= tolower(c);
+				
+			}
+
+			if (punc_check == 1) {
+				break;
+			}
+
+			word_arr[count]= word;
+			count++;
+
 		}
+
 		
-		printf("%s", text);
+		
+		if (punc_check == 1) {
+			punc_check=0;
+			printf("[invalid query]");
+		}
+
+		else {
+			for (int i=0; i< count; i++ ) {
+				printf("%s ", word_arr[i]);
+			}
+		}
+
+		printf("\n");
 		printf(">");
-		free(text);
-			
 	}
 
 	return 0;
